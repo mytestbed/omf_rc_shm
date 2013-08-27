bc79d152cacda1d3916bbd8fa9f20f245b5f497c
Jack C Hong <jhong@nicta.com.au>
2013-08-27 17:55:23 +1000

    Move proxy files to path reflex namespace

--- lib/omf_rc_shm.rb
+++ lib/omf_rc_shm.rb
@@ -1,8 +1,8 @@
 require "omf_rc_shm/version"
-require "omf_rc_shm/resource_proxy/bridge_node"
-require "omf_rc_shm/resource_proxy/crontab"
-
 require "omf_rc_shm/app"
+
+require "omf_rc/resource_proxy/bridge_node"
+require "omf_rc/resource_proxy/crontab"
 
 module OmfRcShm
   def self.app
