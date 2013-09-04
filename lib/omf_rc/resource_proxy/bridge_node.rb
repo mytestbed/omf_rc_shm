83d5a241dea76993dcc1ad941df3086defa888bd
Jack C Hong <jhong@nicta.com.au>
2013-09-04 19:34:30 +1000

    Attempt to create scheduled apps during bootstrap

--- lib/omf_rc/resource_proxy/bridge_node.rb
+++ lib/omf_rc/resource_proxy/bridge_node.rb
@@ -4,7 +4,7 @@ module OmfRc::ResourceProxy::BridgeNode
 
   register_proxy :bridge_node
 
-  property :config_file
+  property :app_definition_file
 
   request :cron_jobs do |node|
     node.children.find_all { |v| v.type =~ /crontab/ }.map do |v|
@@ -13,11 +13,16 @@ module OmfRc::ResourceProxy::BridgeNode
   end
 
   hook :after_initial_configured do |node|
-    OmfRcShm.app.load_definition(node.request_config_file)
+    OmfRcShm.app.load_definition(node.request_app_definition_file)
 
+    # TODO some of the naming will be changed
     OmfRcShm.app.definitions.each do |d|
-      info "Got definition #{d.inspect}"
-      #TODO create crontab proxy based on these defitions
+      info "Got definition #{d.inspect}, now schedule them..."
+      s_app = OmfRc::ResourceFactory.create(:crontab, d)
+
+      OmfCommon.el.after(5) do
+        s_app.configure(state: :scheduled)
+      end
     end
   end
 end
